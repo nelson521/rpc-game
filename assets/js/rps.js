265e4300576527a16f316bb55e23ad89cc35fd35
Stevenson Nelson <sonson521@gmail.com>
2019-05-31 22:59:34 -0400

    added function

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,1 +1,24 @@
-alert('hello.');
+// Global vars
+var rpsJumbotron = document.getElementById('rpsJumbo');
+var rpsCard = document.getElementById('rps-card');
+
+function gameStart(e) {
+  // check if the user presses r, p, or s key.
+  // if they do log the key and hide jumbotron.
+  // any other key should log a message that says 'bad key'.
+  if (e.key === 'r' || e.key === 'p' || e.key === 's') {
+    console.log(e.key);
+    rpsJumbotron.style.display = 'none';
+    rpsCard.style.display = 'flex';
+  } else if (e.key === 'q') {
+    console.log(e.key);
+    rpsJumbotron.style.display = 'block';
+    rpsCard.style.display = 'none';
+  }
+
+  else {
+    console.error('You pressed wrong key. Try again!');
+  }
+}
+// the game starts  
+document.onkeyup = gameStart; 
